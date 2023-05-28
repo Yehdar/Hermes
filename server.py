3615d51b0a922105c5dc513cbe58d7d0a37db74a
Yehdar <p.radhey06@gmail.com>
2023-05-28 12:19:11 -0400

    forgot to add threading for each client connected

--- server.py
+++ server.py
@@ -17,6 +17,7 @@ def broadcast(message):
         client.send(message)
 
 def handle(client):
+    # keeps sending messages until the client is removed
     while true:
         try:
             message = client.recv(1024)
@@ -30,15 +31,25 @@ def handle(client):
             break
 
 def receive():
+    # a while loop to accept every client. NO AUTHENTICATION BTW lol
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
+        # asks user to enter nickname, recieves it, and then store it in the clients list
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
+        # let everyone know a new client joined on the server
         print(f'Nickname of the client is {nickname}!')
         broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        # lets client know they connected
         client.send('Connected to the server!'.encode('ascii'))
+
+        # starts a new thread each time to manage a new client. Better this way because 
+        # it allows for multiple messages to be sent at the same time, rather than waiting for one to go at a time
+        
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
