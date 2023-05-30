5b39564a5a4526f42c193979e3f224e1e8c0f2b0
Yehdar <p.radhey06@gmail.com>
2023-05-30 12:53:02 -0400

    added recieve function to give client perms to set nickname and send messages until an error occurs

--- client.py
+++ client.py
@@ -1,5 +1,23 @@
 import socket
 import threading
 
+nickname = input("Choose a nickname: ")
+
+# user joins
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
+
+# while loop to allow user to keep sending messages until it fails
+def recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            # set nickname
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
